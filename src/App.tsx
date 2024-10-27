6d969741ec5328aa53a1de30fb2db3065c23dc29
SophieRF <sofiaferraro2110@gmail.com>
2024-10-27 18:53:37 -0300

    upload de sucursalView

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import { useState } from 'react'
+import { SucursalView } from "./SucursalView/SucursalView"
+import { ISucursal } from "./types/dtos/sucursal/ISucursal";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const unaSucursal: ISucursal = {
+    id: 1,
+    nombre: "Sucursal Centro",
+    empresa: {
+      id: 1,
+      nombre: "Empresa Ejemplo",
+      razonSocial: "",
+      cuit: 0,
+      logo: null,
+      sucursales: [],
+      pais: {
+        nombre: "unPais",
+        id: 987
+      }
+    },
+    domicilio: {
+      id: 123,
+      calle: "unaCalle",
+      numero: 321,
+      cp: 5657,
+      piso: 3, 
+      eliminado: false,
+      nroDpto: 113,
+      localidad: {
+        id: 143,
+        nombre: "unaLocalidad",
+        provincia: {
+          nombre: "unaProvincia",
+          pais: {
+            nombre: "unPais",
+            id: 456
+          },
+          id: 321
+        }
+      }
+    },
+    calle: "otraCalle",
+    latitud: 19.4326,
+    longitud: -99.1332,
+    categorias: [], // Asumiendo que tienes un array de categorías
+    esCasaMatriz: true,
+    horarioApertura: "08:00",
+    horarioCierre: "18:00",
+    eliminado: false,
+    logo: "https://example.com/logo.png" // URL del logo
+};
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SucursalView sucursal={unaSucursal} />
     </>
   )
 }
--- src/SucursalView/SucursalView.tsx
+++ src/SucursalView/SucursalView.tsx
@@ -1,34 +1,54 @@
 import { Card } from "react-bootstrap";
+import styles from "./SucursalView.module.css"
+import { ISucursal } from "../types/dtos/sucursal/ISucursal";
+import { useState } from "react";
 
-export const SucursalView = () => {
+interface SucursalViewProps {
+    sucursal: ISucursal;
+}
+
+export const SucursalView: React.FC<SucursalViewProps> = ({sucursal}) => {
+
+    const {nombre, empresa, domicilio, esCasaMatriz, horarioApertura, horarioCierre, logo} = sucursal;
+
+    const [esVisible, setEsVisible] = useState(true);
+
+    const onClose = ()=> {
+        setEsVisible(false);
+    }
+    if (!esVisible) return null;
+
     return (
-        <Card style={{ width: '18rem', display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div className={styles.cardContainer}>
+            <Card style={{ width: '18rem'}}>
             <Card.Body>
-                <Card.Title style={{ marginBottom: "28px" }}>Sucursal</Card.Title>
+                <Card.Title style={{ marginBottom: "24px" }}>Sucursal</Card.Title>
                 <Card.Text>
                     <p>
-                        <b>Nombre</b>
+                        <b>Nombre</b> {nombre}
                     </p>
                     <p>
-                        <b>Empresa</b>
+                        <b>Empresa</b> {empresa.nombre}
                     </p>
                     <p>
-                        <b>Domicilio</b>
+                        <b>Domicilio</b> {domicilio.calle} {domicilio.numero}
                     </p>
                     <p>
-                        <b>¿Casa matriz?</b>
+                        <b>¿Casa matriz?</b> {esCasaMatriz? "Si" : "No"}
                     </p>
                     <p>
-                        <b>Horario apertura</b>
+                        <b>Horario apertura</b> {horarioApertura}
                     </p>
                     <p>
-                        <b>Horario cierre</b>
+                        <b>Horario cierre</b> {horarioCierre}
                     </p>
                     <p>
-                        <b>Logo</b>
+                        <b>Logo</b> {logo}
                     </p>
                 </Card.Text>
+                <button className= {styles.closeButton} onClick={onClose} >Cerrar</button>
             </Card.Body>
         </Card>
+        </div>
     );
 }
