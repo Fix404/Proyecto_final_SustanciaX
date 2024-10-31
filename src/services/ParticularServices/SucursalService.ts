678d6906a05358592554d5a424b49409474bf31e
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2024-10-31 19:01:19 -0300

    agregando un navbar

--- src/services/ParticularServices/SucursalService.ts
+++ src/services/ParticularServices/SucursalService.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import { ISucursal } from "../../types/dtos/sucursal/ISucursal";
-import { BackendClient } from "../BackendClient";
-
-export class SucursalService extends BackendClient<ISucursal> {}
-=======
 import { ICreateSucursal } from "../../types/dtos/sucursal/ICreateSucursal";
 import { ISucursal } from "../../types/dtos/sucursal/ISucursal";
 import { IUpdateSucursal } from "../../types/dtos/sucursal/IUpdateSucursal";
 import { BackendClient } from "../BackendClient";
 
 export class SucursalService extends BackendClient<ICreateSucursal|IUpdateSucursal|ISucursal> {}
->>>>>>> 73ff9feea0854e6fcd6eb876815746b8bd02922b
