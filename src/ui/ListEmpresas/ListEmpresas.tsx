c1e523e5f17ef0eb4ed04270b45e2006d3bfa308
Camila <irisarricamila96@gmail.com>
2024-11-02 23:15:37 -0300

    Agregamos CardEmpresa

---
+++ src/ui/ListEmpresas/ListEmpresas.tsx
@@ -0,0 +1,22 @@
+
+import { FC } from "react"
+import styles from "./ListSucursales.module.css"
+import { CardEmpresa } from "../CardEmpresa/CardEmpresa"
+import { IEmpresa } from "../../types/dtos/empresa/IEmpresa"
+
+interface IListEmpresas{
+   empresas:IEmpresa[]
+}
+
+export const ListEmpresas:FC<IListEmpresas> = ({empresas}) => {
+  return (
+    <div className={styles.contenedorPrincipalListaEmpresas}>
+        <div className={styles.contenedorListaEmpresa}>
+            {empresas.map((empresa) => (
+                <CardEmpresa empresa={empresa} key={empresa.id}/>
+            ))}
+        </div>
+
+    </div>
+  )
+}
