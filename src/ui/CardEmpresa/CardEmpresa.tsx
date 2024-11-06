a181beb1a565680bc463e8b23669cae1ffab735e
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2024-11-05 21:30:10 -0300

    Botones copados que seguramente llegarán a la versión final

--- src/ui/CardEmpresa/CardEmpresa.tsx
+++ src/ui/CardEmpresa/CardEmpresa.tsx
@@ -1,34 +1,69 @@
-import { Card } from "react-bootstrap"
-import { IEmpresa } from "../../types/dtos/empresa/IEmpresa"
-import { FC } from "react"
-import { useAppDispatch} from "../../hooks/redux"
-import { removeEmpresaElementActive, setEmpresaElementActive } from "../../redux/slices/EmpresasReducer"
+import { Button, Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { IEmpresa } from "../../types/dtos/empresa/IEmpresa";
+import { FC } from "react";
+import { useAppDispatch } from "../../hooks/redux";
+import {
+  removeEmpresaElementActive,
+  setEmpresaElementActive,
+} from "../../redux/slices/EmpresasReducer";
+import styles from "./CardEmpresa.module.css";
 
-interface ICardEmpresa{
-    empresa:IEmpresa
+interface ICardEmpresa {
+  empresa: IEmpresa;
 }
 
-
-export const CardEmpresa:FC<ICardEmpresa> = ({empresa}) => {
-  const dispatch=useAppDispatch();
-  const handleEmpresaActiva=()=>{
-    dispatch(removeEmpresaElementActive())
-    dispatch(setEmpresaElementActive({element:empresa}))
-  }
+export const CardEmpresa: FC<ICardEmpresa> = ({ empresa }) => {
+  const dispatch = useAppDispatch();
+  const handleEmpresaActiva = () => {
+    dispatch(removeEmpresaElementActive());
+    dispatch(setEmpresaElementActive({ element: empresa }));
+  };
   return (
     <>
-    {empresa && <div>
-      <Card style={{ width: '12rem' }} onClick={handleEmpresaActiva}>
-    <Card.Img variant="top" src="holder.js/100px180" />
-    <Card.Body>
-      <Card.Title>{empresa.nombre}</Card.Title>
-      <Card.Text>
-        {empresa.cuit}
-        {empresa.razonSocial}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  </div>}
+      {empresa && (
+        <div>
+          <Card style={{ width: "12rem" }} onClick={handleEmpresaActiva}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>{empresa.nombre}</Card.Title>
+              <Card.Text>
+                {empresa.cuit}
+                <br />
+                {empresa.razonSocial}
+              </Card.Text>
+              <Card.Footer className={styles.cardEmpresaFooterContainer}>
+                <OverlayTrigger
+                  placement="bottom"
+                  delay={{ show: 250, hide: 400 }}
+                  overlay={
+                    <Tooltip id="button-tooltip-edit-empresa">
+                      Editar Empresa
+                    </Tooltip>
+                  }
+                >
+                  <Button variant="primary">
+                    <span className="material-symbols-outlined">edit</span>
+                  </Button>
+                </OverlayTrigger>
+                <br />
+                <OverlayTrigger
+                  placement="bottom"
+                  delay={{ show: 250, hide: 400 }}
+                  overlay={
+                    <Tooltip id="button-tooltip-delete-empresa">
+                      Eliminar Empresa
+                    </Tooltip>
+                  }
+                >
+                  <Button variant="danger">
+                    <span className="material-symbols-outlined">delete</span>
+                  </Button>
+                </OverlayTrigger>
+              </Card.Footer>
+            </Card.Body>
+          </Card>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
--- src/ui/CardSucursal/CardSucursal.tsx
+++ src/ui/CardSucursal/CardSucursal.tsx
@@ -1,41 +1,64 @@
-import { Button, Card } from "react-bootstrap"
-import { ISucursal } from "../../types/dtos/sucursal/ISucursal"
-import { FC } from "react"
-import { useNavigate } from "react-router-dom"
-import styles from "./CardSucursal.module.css"
+import { Button, Card, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { ISucursal } from "../../types/dtos/sucursal/ISucursal";
+import { FC } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./CardSucursal.module.css";
 
-interface ICardSucursal{
-    sucursal:ISucursal
+interface ICardSucursal {
+  sucursal: ISucursal;
 }
 
+export const CardSucursal: FC<ICardSucursal> = ({ sucursal }) => {
+  const navigate = useNavigate();
 
-export const CardSucursal:FC<ICardSucursal> = ({sucursal}) => {
-
-  const navigate=useNavigate();
-
-    const handleNavigateAdmin= () => {
-        navigate("/admin");
-    }
+  const handleNavigateAdmin = () => {
+    navigate("/admin");
+  };
 
   return (
     <div>
-          <Card className={styles.card} >
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>{sucursal.nombre}</Card.Title>
-              <Card.Text>
-                Horario de apertura: {sucursal.horarioApertura}
-                <br></br>
-                Horario de cierre: {sucursal.horarioCierre}
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <Button variant="warning" onClick={handleNavigateAdmin}>Admin</Button>
-                <Button variant="primary">Ver</Button>
-                <Button variant="secondary">Editar</Button>
-                
-            </Card.Footer>
-          </Card>
+      <Card className={styles.card}>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>{sucursal.nombre}</Card.Title>
+          <Card.Text>
+            Horario de apertura: {sucursal.horarioApertura}
+            <br></br>
+            Horario de cierre: {sucursal.horarioCierre}
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer className={styles.cardSucursalFooterContainer}>
+          <OverlayTrigger
+            placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={
+              <Tooltip id="button-tooltip-admin">Administrar Sucursal</Tooltip>
+            }
+          >
+            <Button variant="warning" onClick={handleNavigateAdmin}>
+              <span className="material-symbols-outlined">apartment</span>
+            </Button>
+          </OverlayTrigger>
+          <OverlayTrigger  placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={
+              <Tooltip id="button-tooltip-ver">Ver Sucursal</Tooltip>
+            }>
+          <Button variant="primary">
+            <span className="material-symbols-outlined">table_eye</span>
+          </Button>
+          </OverlayTrigger>
+          <OverlayTrigger  placement="bottom"
+            delay={{ show: 250, hide: 400 }}
+            overlay={
+              <Tooltip id="button-tooltip-eliminar">Editar Sucursal</Tooltip>
+            }>
+          <Button variant="secondary">
+            <span className="material-symbols-outlined">edit</span>
+          </Button>
+          </OverlayTrigger>
+        </Card.Footer>
+      </Card>
     </div>
-  )
-}
+  );
+};
