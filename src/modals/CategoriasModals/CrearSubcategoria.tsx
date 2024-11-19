24e70b813b849c3e0e5fcb4d8c320960aea6bea9
SophieRF <sofiaferraro2110@gmail.com>
2024-11-19 17:22:32 -0300

    updates de categorias

---
+++ src/modals/CategoriasModals/CrearSubcategoria.tsx
@@ -0,0 +1,98 @@
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Modal, Button, Form } from "react-bootstrap";
+import { useAppDispatch } from "../../hooks/redux";
+import { removeCategoriaElementActive } from "../../redux/slices/CategoriaReducer";
+import { CategoriaService } from "../../services/ParticularServices/CategoriaService";
+import { ICreateCategoria } from "../../types/dtos/categorias/ICreateCategoria";
+import styles from "./CategoriaModal.module.css"
+
+interface IPropsCreateSubcategoria {
+    getCategorias: Function,
+    openModal: boolean;
+    setOpenModal: (state: boolean) => void;
+    idCategoriaPadre: number;
+}
+
+export const CrearSubcategoria = ({
+        getCategorias,
+        openModal,
+        setOpenModal,
+        idCategoriaPadre
+    }: IPropsCreateSubcategoria) => {
+        
+    const initialValues: ICreateCategoria = {
+        denominacion: "",
+        idEmpresa: 1, //RESOLVER ACCESO A EMPRESA ID y CATEGORIA PADRE ID
+        idCategoriaPadre: idCategoriaPadre,
+    }
+
+    const apiCategoria = new CategoriaService("api/categorias/create");
+    const dispatch = useAppDispatch();
+
+    const handleClose = () => {
+        setOpenModal(false);
+        dispatch(removeCategoriaElementActive())
+    }
+
+    return (
+        <div>
+            <Modal
+                show={openModal}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+                data-bs-theme="dark"
+                size="lg"
+                id={"modal"}
+            >
+                <Modal.Header className={styles.modalCategoriaTitulo}   >
+                    <Modal.Title>Crear Sub-Categoría</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        validationSchema={Yup.object({
+                            denominacion: Yup.string().required("Campo requerido"),
+                        })}
+                        initialValues={initialValues}
+                        enableReinitialize={true}
+                        onSubmit={async (values: ICreateCategoria) => {
+                            await apiCategoria.post(values);
+                            getCategorias();
+                            handleClose();
+                        }}
+                    >
+                        {({ values, handleChange, handleSubmit }) => (
+                            <>
+                                <Form onSubmit={handleSubmit} >
+                                        <div>
+                                            <Form.Group className="mb-3" controlId="denominacionCategoria">
+                                                <Form.Control
+                                                    type="text"
+                                                    placeholder="Denominación"
+                                                    autoFocus
+                                                    name="denominacion"
+                                                    onChange={handleChange}
+                                                    value={values.denominacion}
+                                                />
+                                            </Form.Group>
+                                        </div>
+                                    <div className={styles.modalCategoriaBotones}>
+                                        <Button variant="custom" className={styles.modalBoton} onClick={handleClose}>
+                                            Cancelar
+                                        </Button>
+                                        <Button variant="custom" className={styles.modalBoton} type="submit">
+                                            Aceptar
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </>
+                        )}
+                    </Formik>
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
+};
+
---
+++ src/modals/CategoriasModals/EditarCategoria.tsx
@@ -0,0 +1,112 @@
+import { Button, Form, Modal } from "react-bootstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { useAppDispatch, useAppSelector } from "../../hooks/redux";
+import { IUpdateCategoria } from "../../types/dtos/categorias/IUpdateCategoria";
+import { CategoriaService } from "../../services/ParticularServices/CategoriaService";
+import { removeCategoriaElementActive } from "../../redux/slices/CategoriaReducer";
+import styles from "./CategoriaModal.module.css"
+
+interface IPropsUpdateCategoria {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    getCategorias: Function;
+    openModal: boolean;
+    setOpenModal: (state: boolean) => void;
+}
+
+export const EditarCategoria = ({
+    getCategorias,
+    openModal,
+    setOpenModal,
+}: IPropsUpdateCategoria) => {
+    const categoriaActiva = useAppSelector(
+        (state) => state.categoriaReducer.elementActive 
+    )!;
+    //const empresaActiva = categoriaActiva?.sucursales?.[0]?.empresa;
+    const initialValues: IUpdateCategoria = {
+        id: categoriaActiva?.id,
+        denominacion: categoriaActiva?.denominacion,
+        eliminado: categoriaActiva?.eliminado,
+        idCategoriaPadre: null,
+        idSucursales: [],//categoriaActiva?.sucursales.map((sucursal)=>sucursal.id),
+        idEmpresa: 1, //NO SE RENDERIZA UTILIZANDO categoriaActiva.sucursales[0].empresa.id
+    
+    /*    id: elementoActivo.id,
+            denominacion: values.denominacion,
+            idSucursales: elementoActivo.sucursales.map((el) => el.id),
+            eliminado: elementoActivo.eliminado,
+            idEmpresa: empresaActiva!.id,
+            idCategoriaPadre: null, */
+    };
+
+    const apiCategoria = new CategoriaService("api/categorias/update");
+    const dispatch = useAppDispatch();
+
+    const handleClose = () => {
+        setOpenModal(false);
+        dispatch(removeCategoriaElementActive());
+    };
+
+    return (
+        <>
+            <Modal
+                show={openModal}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+                centered
+                data-bs-theme="dark"
+                size="lg"
+                id={"modal"}
+            >
+                <Modal.Header
+                    className={styles.modalCategoriaTitulo}
+                >
+                    <Modal.Title>Editar Categoria</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Formik
+                        validationSchema={Yup.object({
+                            denominacion: Yup.string().required("Campo requerido"),
+                        })}
+                        initialValues={initialValues}
+                        enableReinitialize={true}
+                        onSubmit={async (values: IUpdateCategoria) => {
+                            await apiCategoria.put(categoriaActiva.id, values);
+                            getCategorias();
+                            handleClose();
+                        }}
+                    >
+                        {({ values, handleChange, handleSubmit }) => (
+                            <>
+                                <Form onSubmit={handleSubmit} >
+                                        <div>
+                                            <Form.Group className="mb-3" controlId="denominacion">
+                                                <Form.Control
+                                                    type="text"
+                                                    placeholder="Denominación"
+                                                    autoFocus
+                                                    name="denominacion"
+                                                    onChange={handleChange}
+                                                    value={values.denominacion}
+                                                />
+                                            </Form.Group>
+                                        </div>
+                                    <div className={styles.modalCategoriaBotones}>
+                                        <Button variant="custom" className={styles.modalBoton} onClick={handleClose}>
+                                            Cancelar
+                                        </Button>
+                                        <Button variant="custom" className={styles.modalBoton} type="submit">
+                                            Aceptar
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </>
+                        )}
+                    </Formik>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+};
+
