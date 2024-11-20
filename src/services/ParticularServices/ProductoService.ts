f5ae81fc15ac049c1cab54938874c9a3ca2874a2
Maximiliano Niscola <niscolamaximiliano@gmail.com>
2024-11-20 17:48:09 -0300

    paginado de productos según página desde la api. tiene dos errores en el bodyAdmin

--- src/services/ParticularServices/ProductoService.ts
+++ src/services/ParticularServices/ProductoService.ts
@@ -3,4 +3,17 @@ import { BackendClient } from "../BackendClient";
 import { IUpdateProducto } from "../../types/dtos/productos/IUpdateProducto";
 import { IProductos } from "../../types/dtos/productos/IProductos";
 
-export class ProductoService extends BackendClient<ICreateProducto|IUpdateProducto|IProductos> {}
+export class ProductoService extends BackendClient<ICreateProducto|IUpdateProducto|IProductos> {
+    getProductosBySucursalId = async (idSucursal:Number, page:Number, size:Number):Promise<IProductos[]> => {
+        try{
+            const response=await fetch(`${this.baseUrl}pagedPorSucursal/${idSucursal}?page=${page}&size=${size}`)
+            if(!response) {
+                throw new Error("Error al traer los productos paginados")}
+            const data =await response.json()
+            return data;
+        }catch(error){
+            console.log("Error al traer los productos");
+            throw error
+        }
+    }
+}
